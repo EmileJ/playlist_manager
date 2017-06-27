18b6a939503b321ffb2aaf5a997bd905a88f0f8e
Emile-home <emile.josso@hotmail.fr>
2017-06-27 19:16:02 +0200

    Created a new file containing the custom errors

---
+++ error.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+"""This file describes all the custom errors the program could encounter
+"""
+
+
+
+
+class FolderError(Exception):
+    """Unvalid folder selected
+    """
+    def __init__(self, message):
+    	super().__init__(message)
+
+class FileError(Exception):
+	"""Unvalid file selected
+	"""
+	def __init__(self, message):
+		super().__init__(message)
